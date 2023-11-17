1722db9fbb82f1f3bfd96ec9fcbdadc8a0db1bcd
Xiaohan Du <dux18@cardiff.ac.uk>
2023-11-17 10:31:39 +0000

    Initial commit

---
+++ src/test/java/com/example/backendchallange/BackendChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
