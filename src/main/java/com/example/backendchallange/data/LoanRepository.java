2ef94c63be77852c47cdf8277041eb2a8ea270a5
Xiaohan Du <dux18@cardiff.ac.uk>
2023-11-18 17:16:41 +0000

    Add monthly payment and loans calucation in repo

--- src/main/java/com/example/backendchallange/data/LoanRepository.java
+++ src/main/java/com/example/backendchallange/data/LoanRepository.java
@@ -1,9 +1,12 @@
 package com.example.backendchallange.data;
 
 import com.example.backendchallange.domain.Loan;
+import com.example.backendchallange.domain.Schedule;
 
 import java.util.List;
 
 public interface LoanRepository {
     List<Loan> getLoans();
+    Double calculateMonthlyPayment(Schedule schedule);
+    List<Loan> calculateLoans(Schedule schedule);
 }
--- src/main/java/com/example/backendchallange/data/LoanRepositoryImpl.java
+++ src/main/java/com/example/backendchallange/data/LoanRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.example.backendchallange.data;
 
 import com.example.backendchallange.domain.Loan;
+import com.example.backendchallange.domain.Schedule;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -31,5 +33,41 @@ public class LoanRepositoryImpl implements LoanRepository {
     public List<Loan> getLoans() {
         String getLoanSQL = "SELECT * FROM loan";
         return loanJdbcTemplate.query(getLoanSQL, loanRowMapper);
+    }
+
+    @Override
+    public Double calculateMonthlyPayment(Schedule schedule) {
+        Double monthlyInterestRate = schedule.getYearlyInterestRate() / 12;
+        Double numerator;
+        Double denominator;
+        if (schedule.getBalloonPayment() == 0) {
+            numerator = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), schedule.getRepayMonths());
+            denominator = Math.pow((1 + monthlyInterestRate), schedule.getRepayMonths());
+        } else {
+            numerator = schedule.getAssetCost() - (schedule.getBalloonPayment() / Math.pow((1 + monthlyInterestRate), schedule.getRepayMonths()));
+            denominator = monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), -12));
+        }
+        Double monthlyPayment = schedule.getAssetCost() * (numerator / denominator);
+        return monthlyPayment;
+    }
+
+    @Override
+    public List<Loan> calculateLoans(Schedule schedule) {
+        Double payment = calculateMonthlyPayment(schedule);
+        List<Loan> loans = new ArrayList<>();
+        Double monthlyInterestRate = schedule.getYearlyInterestRate() / 12;
+        Double balance = schedule.getAssetCost();
+        for (int i = 1; i <= 12; i++) {
+            Double interest = balance * monthlyInterestRate;
+            Double principal = payment - interest;
+            balance = balance + interest - payment;
+            Loan loan = new Loan();
+            loan.setPayment(payment);
+            loan.setPrincipal(principal);
+            loan.setInterest(interest);
+            loan.setBalance(balance);
+            loans.add(loan);
+        }
+        return loans;
     }
 }
